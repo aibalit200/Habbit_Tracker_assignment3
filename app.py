246beb4df2c5722eb146ed4fd0df0cdc02757e59
aibalit200 <135988332+aibalit200@users.noreply.github.com>
2026-04-26 21:10:43 +0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, request, redirect, url_for
+import database as db
+
+app = Flask(__name__)
+@app.route('/')
+def index():
+    habits = db.get_all_habits()
+    return render_template('index.html',habits=habits)
+@app.route('/add', methods=['POST'])
+def add():
+    name = request.form['name']
+    if name:
+        db.add_habit(name)
+    return redirect(url_for('index'))
+@app.route('/update', methods=['POST'])
+def update():
+    habit_id = request.form['id']
+    new_name = request.form['new_name']
+    db.update_habit(habit_id, new_name)
+    return redirect(url_for('index'))
+@app.route('/delete/<habit_id>')
+def delete(habit_id):
+    habit_id = int(habit_id)
+    db.delete_habit(habit_id)
+    
+if __name__ == '__main__':
+    db.init_db()
+    app.run(debug=True)
