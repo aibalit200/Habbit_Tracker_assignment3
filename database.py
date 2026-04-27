fb0445896a678adc12784191065f9bb297f8ec89
Adler-ton <mfeof12@gmail.com>
2026-04-27 23:01:36 +0500

    Enhance habits management with additional fields and methods
    Refactor habits database functions to include description, day, and status fields. Add complete and restore functionality for habits.

--- database.py
+++ database.py
@@ -1,24 +1,26 @@
 import sqlite3
-DB_NAME = 'habits.db'
 
+DB_NAME = 'habits.db'
 def init_db():
     with sqlite3.connect(DB_NAME) as conn:
-        conn.execute('CREATE TABLE IF NOT EXISTS habits (id INTEGER PRIMARY KEY, name TEXT)')
-
-def get_all_habits():
+        conn.execute('CREATE TABLE IF NOT EXISTS habits (id INTEGER PRIMARY KEY, name TEXT, description TEXT, day TEXT, status INTEGER DEFAULT 0)')
+def get_habits(day, status):
     conn = sqlite3.connect(DB_NAME)
-    habits = conn.execute('SELECT * FROM habits').fetchall()
+    data = conn.execute('SELECT id, name, description, day FROM habits WHERE day=? AND status=?', (day, status)).fetchall()
     conn.close()
-    return habits
-
-def add_habit(name):
+    return data
+def add_habit(name, desc, day):
     with sqlite3.connect(DB_NAME) as conn:
-        conn.execute('INSERT INTO habits (name) VALUES (?)', (name,))
-
-def update_habit(habit_id, new_name):
+        conn.execute('INSERT INTO habits (name, description, day, status) VALUES (?,?,?,0)', (name, desc, day))
+def update_habit(id, name, desc, day):
     with sqlite3.connect(DB_NAME) as conn:
-        conn.execute('UPDATE habits SET name = ? WHERE id = ?', (new_name, habit_id))
-
-def delete_habit(habit_id):
+        conn.execute('UPDATE habits SET name=?, description=?, day=? WHERE id=?', (name, desc, day, id))
+def delete_habit(id):
     with sqlite3.connect(DB_NAME) as conn:
-        conn.execute('DELETE FROM habits WHERE id = ?', (habit_id,))
+        conn.execute('DELETE FROM habits WHERE id=?', (id,))
+def complete_habit(id):
+    with sqlite3.connect(DB_NAME) as conn:
+        conn.execute('UPDATE habits SET status=1 WHERE id=?', (id,))
+def restore_habit(id):
+    with sqlite3.connect(DB_NAME) as conn:
+        conn.execute('UPDATE habits SET status=0 WHERE id=?', (id,))
